a107b864230faebcacc63510728f4b9503adef08
Daniel Hidalgo <hidalgutzu@gmail.com>
2020-08-19 14:01:32 -0500

    botón play video

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -29,3 +29,33 @@ function myFunction() {
    document.getElementById("fecha").innerHTML = ano;
  }
 myFunction();
+
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+player = new YT.Player('player', {
+   height: '390',
+   width: '640',
+   videoId: YOUR_ID_HERE,
+   playerVars: {
+      controls: 0,
+      showinfo:0,
+      rel:0,
+   },
+   events: {
+      'onReady': onPlayerReady,
+   }
+});
+}
+
+
+//Youtube Modal script
